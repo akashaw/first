b16b118d4a21d4409831e15f3740450a0ba8d654
akashaw <akash98313@gmail.com>
2020-05-02 07:37:14 +0530

    web devemopment project added

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
-from itertools import product as p
-a=(input())
-print(a)
-b=list(p(a,repeat=10))
-for i in b:
-	print(''.join(i))
-print(len(b))
+def func(e,power,bonus):
+	for i in range()
+	power.sort()
+	count=0
+	for i in range(len(power)):
+		if e>power[i]:
+			count+=1
+			e+=bonus[i]
+		else:
+			break
+	return count
+
+print(func(123,[78,130],[10,0]))
